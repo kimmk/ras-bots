72471a7da132f590ae322f7706523957cfb0ce21
Daniel Montero <dmontero93@gmail.com>
2021-04-27 21:03:43 +0300

    control machine

--- src/ctrl_loop/control_loop.py
+++ src/ctrl_loop/control_loop.py
@@ -1,51 +1,92 @@
-#!/usr/bin/python3
-import time
-
-""" Scripts """
-from scripts import controls
-""" ROS """
-import rospy
-from sensor_msgs.msg import CompressedImage
-from std_msgs.msg import Empty
-from std_msgs.msg import UInt8
-from geometry_msgs.msg import Twist
-""" Others """
-from control_state import ControlState 
+import os
+import sys
+import signal
 import cv2
+import imutils
+from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
+import matplotlib.pyplot as plt
+import rospy
+from sensor_msgs.msg import Image
+from std_msgs.msg import Float64
+from geometry_msgs.msg import Pose.
+from scripts.img_recon import GateDetector
+from scripts.controls import *
 
-""" ROS TOPIC DEFINITIONS - PUBLISHERS """
-cmd_vel = rospy.Publisher('/tello/cmd_vel', Twist, queue_size=2)
-cmd_takeoff = rospy.Publisher('/tello/takeoff', Empty, queue_size=1)
-cmd_land = rospy.Publisher('/tello/land', Empty, queue_size=1)
+decisionNone = 0
+decisionCorrection = 1
+decisionGo = 2
+magicNumber = 10
 
-""" ROS TOPIC DEFINITIONS - SUBSCRIBERS """
+class ControlState:
+    def __init__():
+        rospy.Subscriber("/image", Image, self.camera_callback)  # Tello camera 
+        self.cmd_pub = rospy.Publisher('/tello/cmd_vel', Twist, queue_size=1)
+        self.gateDetector = GateDetector()
+        self.decisionTally = np.array([0,0,0])
+        self.decisionTotals = 0
+        self.lastGate = [0,0,(0,0)]
 
+    def move_around_gate(self, alpha, gate_x, gate_z, gate_dist):
+        #see what input is like, the convert alpha to deg, with 0 = center, +90 = left, -90 = right
+        #convert x and z to % of image
+        #set speeds to 0
+        sx,sy,sz,saz = 0,0,0,0
+        #TODO check signs for all
+        sy += alpha  #at 90 deg, speed=max=pi/2
+        saz += alpha
+        sy += gate_x
+        sz += -gate_z
+        sx = min(gate_dist - self.target_dist, 1) #max speed =1
+        self.cmd_pub.publish(controls.control(y = sx, x = sy, z = sz, az = saz))
+        time.sleep(0.5)
+        self.cmd_pub.publish(controls.hold())
 
-""" CONTROL LOOP CODE """
-def main():
-    print('CTRL-LOOP INIT')
-    ## THIS IS WHERE THE CODE FOR CONTROLLING THE STATES SHOULD BE, AND STATES SHOULD BE DEFINITIONS THAT CALL FUNCTIONS IN THE SCRIPT FOLDER
+    def go_trough_gate(self):
+        sx = 1
+        self.cmd_pub.publish(controls.control(y=sx))
+        time.sleep(2)
+        self.cmd_pub.publish(controls.hold())
+        time.sleep(0.5)
+        return 1
+        
+    def camera_callback(self, img):
+        gate = self.gateDetector.image_processing(img)
+        decision = decisionNone
+        dWeight = 1
 
-    ## Code is currently just for take off and landing
-    time.sleep(5)
-    controls.takeoff(cmd_takeoff)
-    time.sleep(5)
+        if gate is None:
+            pass
+        else:
+            self.lastGate = gate
+            angle, dist, (x, y) = gate
+            ######## Decider
+            if abs(x)<0.05 and abs(y)<0.05 and abs(angle)<0.1 and abs(dist-self.target_dist)<0.1:
+                # foo = self.go_trough_gate()
+                # print("gogogo")
+                decision = decisionGo
+            else:
+                # self.move_around_gate(angle,x,y,dist)
+                # print("adjust")
+                decision = decisionCorrection
+                dWeight = 2
+        self.decisionTotals += 1
+        self.decisionTally[decision] += dWeight
+        if self.decisionTotals < magicNumber
+            return
 
-    controls.control(cmd_vel, y=1)
-    time.sleep(2)
-    controls.control(cmd_vel)
-
-    time.sleep(5)
-    controls.land(cmd_land)
-
-    
-
+        decision = np.argmax(self.decisionTally)
+        self.decisionTally = np.array([0,0,0])
+        self.decisionTotals = 0
+        angle, dist, (x, y) = self.lastGate
+        if decision == decisionNone:
+            ##this is the part where we pretend that we know what we are doing
+        elif decision == decisionCorrection:
+            self.move_around_gate(angle,dist,x,y)
+        else: 
+            self.go_trough_gate()
 
 if __name__ == '__main__':
-    try:
-        # Initialize the ROS node in case the python is initialized from here
-        rospy.init_node('jetbot_control', anonymous=True)
-        main()
-    except BaseException:
-        traceback.print_exc()
+    signal.signal(signal.SIGINT, handle_exit)
+    rospy.init_node('gate_detector', anonymous=True)
+    rospy.spin()
--- src/scripts/controls.py
+++ src/scripts/controls.py
@@ -6,17 +6,17 @@ from geometry_msgs.msg import Twist
 """
 use case:
 import controls
-"""
+# """
 
-def takeoff(cmd_takeoff):
-    print("TAKEOFF")
-    cmd_takeoff.publish(Empty())
+# def takeoff(cmd_takeoff):
+#     print("TAKEOFF")
+#     cmd_takeoff.publish(Empty())
 
-def land(cmd_land):
-    print("LAND")
-    cmd_land.publish(Empty())
+# def land(cmd_land):
+#     print("LAND")
+#     cmd_land.publish(Empty())
 
-def control(cmd_vel,x=0,y=0,z=0,ax=0,ay=0,az=0):
+def control(x=0,y=0,z=0,ax=0,ay=0,az=0):
     msg = Twist()
     msg.linear.x=x
     msg.linear.y=y
@@ -24,23 +24,22 @@ def control(cmd_vel,x=0,y=0,z=0,ax=0,ay=0,az=0):
     msg.angular.x=ax
     msg.angular.y=ay
     msg.angular.z=az
-    #sendVel(msg)
-    cmd_vel.publish(msg)
+    return msg
 
-def rotz(cmd_vel,az):
-    cmd_vel = rospy.Publisher('/tello/cmd_vel', Twist, queue_size=1)
-    msg = Twist()
-    msg.linear.x=0
-    msg.linear.y=0
-    msg.linear.z=0
-    msg.angular.x=0
-    msg.angular.y=0
-    msg.angular.z=az
-    #sendVel(msg)
-    cmd_vel.publish(msg)
+# def rotz(cmd_vel,az):
+#     cmd_vel = rospy.Publisher('/tello/cmd_vel', Twist, queue_size=1)
+#     msg = Twist()
+#     msg.linear.x=0
+#     msg.linear.y=0
+#     msg.linear.z=0
+#     msg.angular.x=0
+#     msg.angular.y=0
+#     msg.angular.z=az
+#     #sendVel(msg)
+#     cmd_vel.publish(msg)
 
 #redundant but nice to have
-def hold(cmd_vel):
+def hold():
     msg = Twist()
     msg.linear.x=0
     msg.linear.y=0
@@ -48,4 +47,4 @@ def hold(cmd_vel):
     msg.angular.x=0
     msg.angular.y=0
     msg.angular.z=0
-    cmd_vel.publish(msg)
+    return msg
