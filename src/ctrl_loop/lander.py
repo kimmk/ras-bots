f5c79fa94b42afb38c27818307b05c90492c590f
kimmk <kimmo.korpelin@gmail.com>
2021-05-08 05:51:23 +0300

    add initial lander code

---
+++ src/ctrl_loop/lander.py
@@ -0,0 +1,111 @@
+import numpy as np
+import cv2
+import imutils
+import matplotlib.pyplot as plt
+from simple_pid import PID
+
+# For debugging
+def imshow_grayscale(img):    
+    plt.imshow(img, cmap='gray', vmin=0, vmax=255)
+    plt.show()
+
+def imshow_hsv(img):
+    img = cv2.cvtColor(img, cv2.COLOR_HSV2RGB)
+    plt.imshow(img)
+    plt.show()
+
+def box_center(box):
+    return (box[0]+box[2]//2, box[1]+box[3]//2)
+
+class Lander(object):
+    def __init__(self):
+        self.pid_x = PID(10.0, 0, 3.0)
+        self.pid_y = PID(10.0, 0, 3.0)
+
+    def find_leds(self, img, hsv_min, hsv_max, debug_img=None):
+        debug_img = img.copy()
+
+        # Filter LED color only
+        lower = np.array(hsv_min, dtype="uint8")
+        upper = np.array(hsv_max, dtype="uint8")
+        mask = cv2.inRange(img, lower, upper)
+        img_masked = cv2.bitwise_and(img, img, mask=mask)
+
+        # Take grayscale, apply blur and threshold
+        gray = cv2.split(img_masked)[2]
+        blurred = cv2.medianBlur(gray, ksize=5)
+        thresh = cv2.threshold(blurred, 5, 255, cv2.THRESH_BINARY)[1]
+
+        # Find contours
+        cnts = cv2.findContours(thresh, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        im_cnts = imutils.grab_contours(cnts)
+
+        # Return features that are large enough to be candidates
+        leds = []
+        for points in im_cnts:
+            led_box = cv2.boundingRect(points)
+            x,y,w,h = led_box
+            if w*h > 20:
+                leds.append(led_box)
+                if debug_img is not None:
+                    cv2.rectangle(debug_img, (x,y), (x+w,y+h), (100,255,255), 2)
+
+        # Sort by size
+        leds.sort(key=lambda box: box[2]*box[3], reverse=True)
+
+        return leds
+
+    # img:              landing camera image in cv2 HSV format
+    # cmd_vel:          velocity topic for Tello drone
+    # led_a, led_b:     HSV color ranges for front led (led_a) and back led (led_b)
+    # land_pos:         target landing position in image, in percentages of X and Y
+    # debug_img:        if set, debug info will be drawn to this image
+    def land_update(self, img, cmd_vel, led_a, led_b, land_pos=(0.5,0.5), debug_img=None):
+        self.last_vel_cmd = cmd_vel
+
+        # Find LED candidate positions
+        leds_a = self.find_leds(img, led_a[0], led_a[1], debug_img)
+        leds_b = self.find_leds(img, led_b[0], led_b[1], debug_img)
+        if len(leds_a) == 0 or len(leds_b) == 0:
+            self.reset_vel()
+            return
+
+        # Right now, just pick the largest leds. Maybe could do some fancy size matching here?
+        a_box = leds_a[0]
+        b_box = leds_b[0]
+        a_pos = box_center(a_box)
+        b_pos = box_center(b_box)
+
+        # Estimate craft position, height, angle
+        x = (a_pos[0] + b_pos[0]) // 2
+        y = (a_pos[1] + b_pos[1]) // 2
+        h = 0
+        a = 0
+
+        # Calculate initial velocity vector
+        self.pid_x.setpoint = img.shape[0] * land_pos[0]
+        self.pid_y.setpoint = img.shape[1] * land_pos[1]
+      
+        dx = self.pid_x(x)
+        dy = self.pid_x(y)
+        v = np.array([dx,dy])
+
+        # Normalize velocity vector
+        norm = np.linalg.norm(v)
+        v = v / norm
+
+        # Apply height modifier and generic velocity multiplier
+        v = (1-(1/h)) * 1.0 * v
+
+        # TODO: Rotate velocity vector according to the craft angle
+
+        return v
+
+if __name__ == '__main__':
+    lander = Lander()
+    img = cv2.imread("test.png")
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    debug_img = img_hsv.copy()
+    leds = lander.find_leds(img_hsv, [0,240,110], [3,255,255], debug_img=debug_img)
+    imshow_hsv(debug_img)
+    print(leds)
---
+++ src/ctrl_loop/landing_test.py
@@ -0,0 +1,55 @@
+import sys
+import signal
+import numpy as np
+import cv2
+import imutils
+from cv_bridge import CvBridge, CvBridgeError
+import rospy
+from sensor_msgs.msg import Image
+from std_msgs.msg import Empty
+from geometry_msgs.msg import Twist
+import controls
+import lander
+
+bridge = CvBridge()
+
+class LandingTest(object):
+    def __init__(self):
+        rospy.Subscriber("/image", Image, self.camera_callback)  # Tello camera
+        self.cmd_vel = rospy.Publisher('/tello/cmd_vel', Twist, queue_size=1)
+        self.vel_timeout = None
+        self.lander = lander.Lander()
+        self.led_a = [[0,0,0], [0,0,0]]
+        self.led_b = [[0,0,0], [0,0,0]]
+
+    def reset_vel(self):
+        self.cmd_vel.publish(controls.hold())
+
+    def camera_callback(self, msg):
+        img = bridge.imgmsg_to_cv2(msg, "hsv8")
+        debug_img = img.copy()
+        v = self.lander.land_update(img, self.led_a, self.led_b, debug_img=debug_img)
+
+        # Publish velocity command
+        cmd_vel.publish(controls.control(v[0], v[1]))
+        
+        # Setup velocity timeout if set
+        if self.vel_timeout is not None:
+            self.vel_timeout.shutdown()
+        self.vel_timeout = rospy.Timer(rospy.Duration(0.5), self.reset_vel, oneshot=True)
+
+def handle_exit(signum, frame):
+    cmd_vel = rospy.Publisher('/tello/cmd_vel', Twist, queue_size=1)
+    cmd_land = rospy.Publisher('/tello/land', Empty, queue_size=1)
+    rospy.sleep(0.5)
+    print("Landing and reseting velocity")
+    cmd_land.publish(Empty())
+    cmd_vel.publish(controls.hold())
+    rospy.signal_shutdown("Landing and reseting velocity")
+    sys.exit(0)
+
+if __name__ == '__main__':
+    rospy.init_node('landing_test', anonymous=True)
+    signal.signal(signal.SIGINT, handle_exit)
+    landingTest = LandingTest()
+    rospy.spin()
